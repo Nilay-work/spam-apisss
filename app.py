93d7e876f1166b56c81b6823942b15acded58c5a
Nilay-work <cehek18350@aikunkun.com>
2025-11-26 20:06:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -448,8 +448,6 @@ def spam_clan():
         return jsonify({
             "error": "Server error"
         }), 500
-
+# ==== Main ====
 if __name__ == "__main__":
-    import sys
-    port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
